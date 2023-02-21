6311e220a17d085bbd55f70d18ac20af9360a6dd
Angela-Qii <121976089+Angela-Qii@users.noreply.github.com>
2023-02-20 23:19:09 -0800

    Added Chart 2

---
+++ Chart_2.R
@@ -0,0 +1,18 @@
+# Required libraries
+library(dplyr)
+library(plotly)
+library(ggplot2)
+
+# Assigning 2021 Variable
+WHR_2021 <- read.csv("./WHR_2021.csv")
+
+Life_expectancy_per_region <- data.frame(WHR_2021$Regional.indicator, WHR_2021$Healthy.life.expectancy)
+
+# Making graph
+
+ggplot(data = Life_expectancy_per_region, aes(x = as.character(WHR_2021.Regional.indicator), y = WHR_2021.Healthy.life.expectancy, fill = as.character(WHR_2021.Regional.indicator))) +
+  geom_boxplot()+
+  labs( x="Regions", y="Helathy Life expectancy")
+
+#Plotting graph
+Life_expectancy_per_region_boxplot

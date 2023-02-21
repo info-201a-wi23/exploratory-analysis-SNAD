cfb69409020c14d13f09c1ceefedf502b755f20c
stevessuw <121976244+stevessuw@users.noreply.github.com>
2023-02-20 23:52:31 -0800

    Update Summary Table.R

--- Summary Table.R
+++ Summary Table.R
@@ -6,6 +6,6 @@ summary_table <- WHR_2021 %>% group_by(Regional.indicator) %>% summarise(
   total_countries = n(), 
   avg_ladder_score <- round(mean(Ladder.score), 2),
   avg_freedom_score <- round(mean(Freedom.to.make.life.choices), 2),
-  avg_gtp_score <- round(mean(Logged.GDP.per.capita), 2), 
+  avg_gdp_score <- round(mean(Logged.GDP.per.capita), 2), 
   avg_lifespan_score <- round(mean(Healthy.life.expectancy), 2),
 )
